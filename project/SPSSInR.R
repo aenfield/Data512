a8bba530972ada0ea0c71848f21c8e29126087ee
Andrew Enfield <aenfield@gmail.com>
2017-11-26 13:28:22 -0800

    Added R code to load SPSS extract from IPUMS CPS and write as CSV, and a bit of sanity check code in project notebook to check the data.

--- project/SPSSInR.R
+++ project/SPSSInR.R
@@ -1,5 +1,19 @@
+library(foreign)
+
 #setwd('~/work/github/Data512/project')
 setwd('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project/Oct16')
-library(foreign)
 d = read.spss('Oct16 public.sav', to.data.frame=TRUE)
+
+labels <- attributes(d)$variable.labels
+write.csv(labels, 'Oct16 public labels.csv')
+
 write.csv(d, 'Oct16 public.csv')
+
+
+setwd('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project')
+d = read.spss('cps_00001.sav', to.data.frame=TRUE)
+
+labels <- attributes(d)$variable.labels
+write.csv(labels, 'cps_00001 labels.csv')
+
+write.csv(d, 'cps_00001.csv')
