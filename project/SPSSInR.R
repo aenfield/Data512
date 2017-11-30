6692f5fa41e05f37f79b2a09ba965c78786d5780
Andrew Enfield <aenfield@gmail.com>
2017-11-30 13:51:44 -0800

    Got population for all cells, after mapping all needed fields - just one; got first total/all-up prediction.

--- project/SPSSInR.R
+++ project/SPSSInR.R
@@ -7,7 +7,7 @@ d = read.spss('Oct16 public.sav', to.data.frame=TRUE)
 labels <- attributes(d)$variable.labels
 write.csv(labels, 'Oct16 public labels.csv')
 
-write.csv(d, 'Oct16 public.csv')
+write.csv(d, 'Oct16 public.csv', row.names = FALSE)
 
 
 setwd('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project')
@@ -16,4 +16,4 @@ d = read.spss('cps_00001.sav', to.data.frame=TRUE)
 labels <- attributes(d)$variable.labels
 write.csv(labels, 'cps_00001 labels.csv')
 
-write.csv(d, 'cps_00001.csv')
+write.csv(d, 'cps_00001.csv', row.names = FALSE)
