9e356b414640b13e2b94a8eb7eb7a714b878ab1e
Andrew Enfield <aenfield@gmail.com>
2017-11-30 16:29:21 -0800

    Tweaked R code to convert addtl CPS data using a function.

--- project/SPSSInR.R
+++ project/SPSSInR.R
@@ -1,19 +1,17 @@
 library(foreign)
 
-#setwd('~/work/github/Data512/project')
-setwd('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project/Oct16')
-d = read.spss('Oct16 public.sav', to.data.frame=TRUE)
+convert_sav_to_csv <- function(workingdir, input_filename, output_filename_prefix) {
+  setwd(workingdir)
+  d = read.spss(input_filename, to.data.frame=TRUE)
+  
+  labels <- attributes(d)$variable.labels
+  write.csv(labels, paste(output_filename_prefix, "labels.csv"))
+  
+  write.csv(d, paste(output_filename_prefix, ".csv", sep=""), row.names = FALSE)
+}
 
-labels <- attributes(d)$variable.labels
-write.csv(labels, 'Oct16 public labels.csv')
+convert_sav_to_csv('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project/Oct16', 'Oct16 public.sav', 'Oct16 public')
+convert_sav_to_csv('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project', 'cps_00002.sav', 'cps_00002')
+convert_sav_to_csv('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project', 'cps_00003.sav', 'cps_00003')
 
-write.csv(d, 'Oct16 public.csv', row.names = FALSE)
 
-
-setwd('~/OneDrive/Personal/School/Master\'s/DATA\ 512/Project')
-d = read.spss('cps_00001.sav', to.data.frame=TRUE)
-
-labels <- attributes(d)$variable.labels
-write.csv(labels, 'cps_00001 labels.csv')
-
-write.csv(d, 'cps_00001.csv', row.names = FALSE)
